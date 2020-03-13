925a189b9017b94e8787afbf3551fde40417ac53
janj <jan.junge@imbus.de>
2020-03-13 14:18:09 +0100

    Updated version number before publishing.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tbcs_rf_wrapper',
-    version='0.14',
+    version='0.15',
     packages=find_packages(exclude=['tests']),
 
     python_requires='>=3.6',
